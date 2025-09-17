62a7e6dc5ffd9d82769d1da9d0f5f2a41a5165b2
Estevao-Bueno <estevao.bueno@estudante.ifms.edu.br>
2025-09-17 11:52:32 -0400

    criação de function handleForm

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,42 @@ type ProdutoType = {
 
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  //const [nome, setNome] = useState('')
   useEffect(() => {
     fetch('/api/produtos')
       .then(res => res.json())
       .then((data) => setProdutos(data))
   }, [])
 
+  function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+  } // precisa dicionar mais linhas
+
   console.log(produtos)
 
     
   return (
     <>
-    <div>Estêvão</div>
+    <div>Cadastro de Produtos</div>
+    <form onSubmit={handleForm}>
+    <input type="text" name="nome" placeholder="Nome" /><br />
+    <input type="number" name="preco" placeholder="Preço" /><br />
+    <input type="text" name="urlfoto" placeholder="URL da Foto" /><br />
+    <textarea name="descricao" placeholder="Descrição"></textarea><br />
+    <button type="submit">Adicionar Produto</button>
+    </form>
+
+    <div>Lista de Produtos</div>
     {
     produtos.map((produto) => (
       <div key={produto._id}>
