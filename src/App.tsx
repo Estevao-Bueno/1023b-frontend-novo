d9ff5138a475bf5322df65dacd69659bdbf94f70
Estevao-Bueno <estevao.bueno@estudante.ifms.edu.br>
2025-10-20 07:21:28 -0400

    validar autorização do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import api from './api/api'
 //useffect
 import { useState, useEffect } from 'react'
 type ProdutoType = {
@@ -11,13 +12,8 @@ type ProdutoType = {
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
   useEffect(() => {
-    fetch('/api/produtos',{
-      headers:{
-        'Authorization': ''
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => setProdutos(data))
+    api.get("/produtos")
+      .then((response) => setProdutos(response.data))
       .catch((error) => console.error('Error fetching data:', error))
   }, [])
   function handleForm(event: React.FormEvent<HTMLFormElement>) {
