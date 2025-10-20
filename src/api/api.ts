97873c29aaaafff99bbcda77aa906d92eba1eabb
Estevao-Bueno <estevao.bueno@estudante.ifms.edu.br>
2025-10-20 07:20:38 -0400

    criar um middware para adicionar o token na requisição

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,10 +8,24 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
-  if (token) {
+  if (token)
     config.headers.Authorization = `Bearer ${token}`
     return config
   })
+
+//Redirecionarpara o Login quando o usuário não estiver autenticado
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log('Teste: Erro na resposta da API:', error)
+    const status = error?.response?.status;
+    if (status === 401 &&!(error?.config?.url?.endsWith('/login'))) {
+      localStorage.removeItem('token')
+      window.location.href = '/login?message=Token Invalido'
+    }
+    return Promise.reject(error)
+  }
+)
 
 
 export default api
